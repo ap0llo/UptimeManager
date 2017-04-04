ce18f6b97526bd3da66392b8f92258e714a99796
Andreas Grünwald <andreasgruenwald@live.de>
2017-04-04 15:45:00 +0200

    Add msbuild.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,4 @@
 @echo off
-
-for /f "usebackq tokens=*" %%i in (`tools\vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-  set InstallDir=%%i
-)
-
-set MSBuildPath="%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe"
-
-%MSBuildPath% ./src/UptimeManager.sln /t:Restore /p:Configuration=Release
-%MSBuildPath% ./src/UptimeManager.sln /t:Build /p:Configuration=Release
+./msbuild ./src/UptimeManager.sln /t:Restore /p:Configuration=Release
+./msbuild ./src/UptimeManager.sln /t:Build /p:Configuration=Release
 

922ccf228523a98dcba9f286b5b9de6ef550cf9d
Andreas Grünwald <andreasgruenwald@live.de>
2017-04-04 17:01:36 +0200

    Automatically make internal members visible to test projects removing the need for a AssemblyInfo file in the projects

--- src/UptimeManager.Client.Powershell/main/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("UptimeManager.Client.Powershell.Test")]
--- src/UptimeManager.Configuration/main/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿
-
-
-using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("UptimeManager.Configuration.Test")]

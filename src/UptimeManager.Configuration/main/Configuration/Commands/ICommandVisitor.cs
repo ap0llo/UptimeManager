3aa4908ce1edb53a80c40d507b2395532143ea28
Andreas Grünwald <andreasgruenwald@live.de>
2017-11-04 20:26:17 +0100

    Update license header

--- src/UptimeManager.Configuration/main/Configuration/Commands/ICommandVisitor.cs
+++ src/UptimeManager.Configuration/main/Configuration/Commands/ICommandVisitor.cs
@@ -1,7 +1,7 @@
-﻿// // -----------------------------------------------------------------------------------------------------------
-// //  Copyright (c) 2015-2016, Andreas Grünwald
-// //  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
-// // -----------------------------------------------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------------------------------------------
+//  Copyright (c) 2015-2017, Andreas Grünwald
+//  Licensed under the MIT License. See LICENSE.txt file in the project root for full license information.  
+// -----------------------------------------------------------------------------------------------------------
 
 
 namespace UptimeManager.Configuration.Commands

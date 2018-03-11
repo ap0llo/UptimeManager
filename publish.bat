5591f571ebd802ffe52ee6d638a709b3fdfcb09d
Andreas Grünwald <andreasgruenwald@live.de>
2018-03-11 10:41:31 +0100

    Exclude UptimeManager.Client.Powershell from publish.bat

--- publish.bat
+++ publish.bat
@@ -12,5 +12,5 @@ CALL %~dp0\msbuild.bat %COMMONPARAMETERS% ^
 CALL %~dp0\msbuild.bat %COMMONPARAMETERS% ^
      %~dp0\src\UptimeManager.Daemon\main\UptimeManager.Daemon.csproj /p:RuntimeIdentifier=win-x64
 
-CALL %~dp0\msbuild.bat %COMMONPARAMETERS% ^
-     %~dp0\src\UptimeManager.Client.Powershell\main\UptimeManager.Client.Powershell.csproj /p:RuntimeIdentifier=win
+REM CALL %~dp0\msbuild.bat %COMMONPARAMETERS% ^
+     REM %~dp0\src\UptimeManager.Client.Powershell\main\UptimeManager.Client.Powershell.csproj /p:RuntimeIdentifier=win
